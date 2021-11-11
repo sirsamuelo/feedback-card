40cec248bfcaaba2862bbc895707a2e9495369bd
ss530eh <samuel.sirotnik@student.tuke.sk>
2021-11-11 15:24:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector(".btn");
+const panel = document.querySelector(".panel");
+const cards = document.querySelectorAll(".card");
+
+let feedback;
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    removeClass();
+    feedback = card.lastElementChild.innerText;
+    card.classList.add("active");
+  });
+});
+
+btn.addEventListener("click", () => {
+  changeBackground();
+});
+
+function changeBackground() {
+  panel.innerHTML = `
+    <div class="changedPanel">
+        <i class='bx bxs-heart'></i>
+        <h3>Thank you for your submit ! </h3>
+        <small>Feedback ${feedback}</small>
+        <p>We appreciate your feedback and work constantly to improve your experience </p>
+        <strong> See you soon !</strong>
+    </div>
+        `;
+}
+
+function removeClass() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+}
